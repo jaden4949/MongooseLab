30950046a23a28ed5644d514cab9502e6f470e4b
jaden4949 <jaden4949@gmail.com>
2024-02-26 11:02:27 -0500

    Lab 1

---
+++ models/Flight.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const flightSchema = new mongoose.Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United'],
+        required: true
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    },
+    flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            return new Date().setFullYear(new Date().getFullYear() + 1);
+        }
+    }
+});
+
+const Flight = mongoose.model('Flight', flightSchema);
+
+module.exports = Flight;
